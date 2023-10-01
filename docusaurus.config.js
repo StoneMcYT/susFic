8620d5c9ce2c4188ff00bca826038e74c3e039ef
StoneMcYT <67421882+StoneMcYT@users.noreply.github.com>
2023-10-01 18:41:02 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,10 +11,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'http://susfic.thatsnek.asia/',
+  url: 'https://susfic.thatsnek.asia/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/susFic/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
